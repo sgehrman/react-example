fcd8747dcf7296b73888f614acd41a2a718c6019
Steve Gehrman <sgehrman@gmail.com>
2020-09-23 02:37:45 -0700

    flex direction

--- src/components/FlexContainer.js
+++ src/components/FlexContainer.js
@@ -8,6 +8,7 @@ function FlexContainer() {
   const [alignItems, setAlignItems] = React.useState('center')
   const [alignContent, setAlignContent] = React.useState('flex-start')
   const [justifyContent, setJustifyContent] = React.useState('flex-start')
+  const [flexDirection, setFlexDirection] = React.useState('row')
 
   const boxes = []
 
@@ -15,6 +16,7 @@ function FlexContainer() {
     boxes.push(<NumberBox key={i} title={i.toString()} />)
   }
 
+  const flexDirectionOptions = ['row', 'column']
   const alignItemsOptions = ['stretch', 'center', 'flex-start', 'flex-end']
   const alignContentsOptions = ['stretch', 'center', 'flex-start', 'flex-end', 'space-between', 'space-around']
   const justifyContentOptions = [
@@ -38,6 +40,9 @@ function FlexContainer() {
       case 'justify-content':
         setJustifyContent(item)
         break
+      case 'flex-direction':
+        setFlexDirection(item)
+        break
       default:
         console.log('onMenuSelect: default case hit.')
         break
@@ -57,6 +62,7 @@ function FlexContainer() {
     alignItems,
     alignContent,
     justifyContent,
+    flexDirection,
   }
 
   return (
@@ -69,6 +75,10 @@ function FlexContainer() {
 
       <div className={styles.centerColumn}>
         <div>
+          <div className={styles.controlMenuItem}>
+            <FlexPopupMenu title="flex-direction" options={flexDirectionOptions} onMenuSelect={onMenuSelect} />
+            <div className={styles.controlMenuValue}>{flexDirection}</div>
+          </div>
           <div className={styles.controlMenuItem}>
             <FlexPopupMenu title="align-items" options={alignItemsOptions} onMenuSelect={onMenuSelect} />
             <div className={styles.controlMenuValue}>{alignItems}</div>
